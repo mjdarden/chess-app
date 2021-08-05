235cb7039333ca3a19647fefdf061e089ee1d878
mjdarden <mjondarden@gmail.com>
2021-08-04 23:14:34 -0500

    made unicode into variables

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
 import './App.css';
 
 function App() {
+  const bKing = '\u265A',
+    bQueen = '\u265B',
+    bBishop ='\u265D',
+    bKnight = '\u265E',
+    bRook = '\u265C',
+    bPawn = '\u265F',
+    wKing = '\u2654',
+    wQueen = '\u2655',
+    wBishop ='\u2657',
+    wKnight = '\u2658',
+    wRook = '\u2656',
+    wPawn = '\u2659'
   return (
     <div className="App">
       <div class="chessboard">
-        <div class="white">&#9820;</div>
-        <div class="black">&#9822;</div>
-        <div class="white">&#9821;</div>
-        <div class="black">&#9819;</div>
-        <div class="white">&#9818;</div>
-        <div class="black">&#9821;</div>
-        <div class="white">&#9822;</div>
-        <div class="black">&#9820;</div>
+        <div class="white">{bRook}</div>
+        <div class="black">{bKnight}</div>
+        <div class="white">{bBishop}</div>
+        <div class="black">{bQueen}</div>
+        <div class="white">{bKing}</div>
+        <div class="black">{bBishop}</div>
+        <div class="white">{bKnight}</div>
+        <div class="black">{bRook}</div>
 
-        <div class="black">&#9821;</div>
-        <div class="white">&#9821;</div>
-        <div class="black">&#9821;</div>
-        <div class="white">&#9821;</div>
-        <div class="black">&#9821;</div>
-        <div class="white">&#9821;</div>
-        <div class="black">&#9821;</div>
-        <div class="white">&#9821;</div>
-
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
-
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
+        <div class="black">{bPawn}</div>
+        <div class="white">{bPawn}</div>
+        <div class="black">{bPawn}</div>
+        <div class="white">{bPawn}</div>
+        <div class="black">{bPawn}</div>
+        <div class="white">{bPawn}</div>
+        <div class="black">{bPawn}</div>
+        <div class="white">{bPawn}</div>
 
         <div class="white"></div>
         <div class="black"></div>
@@ -58,23 +52,41 @@ function App() {
         <div class="black"></div>
         <div class="white"></div>
 
-        <div class="white">&#9817;</div>
-        <div class="black">&#9817;</div>
-        <div class="white">&#9817;</div>
-        <div class="black">&#9817;</div>
-        <div class="white">&#9817;</div>
-        <div class="black">&#9817;</div>
-        <div class="white">&#9817;</div>
-        <div class="black">&#9817;</div>
+        <div class="white"></div>
+        <div class="black"></div>
+        <div class="white"></div>
+        <div class="black"></div>
+        <div class="white"></div>
+        <div class="black"></div>
+        <div class="white"></div>
+        <div class="black"></div>
 
-        <div class="black">&#9814;</div>
-        <div class="white">&#9816;</div>
-        <div class="black">&#9815;</div>
-        <div class="white">&#9813;</div>
-        <div class="black">&#9812;</div>
-        <div class="white">&#9815;</div>
-        <div class="black">&#9816;</div>
-        <div class="white">&#9814;</div>
+        <div class="black"></div>
+        <div class="white"></div>
+        <div class="black"></div>
+        <div class="white"></div>
+        <div class="black"></div>
+        <div class="white"></div>
+        <div class="black"></div>
+        <div class="white"></div>
+
+        <div class="white">{wPawn}</div>
+        <div class="black">{wPawn}</div>
+        <div class="white">{wPawn}</div>
+        <div class="black">{wPawn}</div>
+        <div class="white">{wPawn}</div>
+        <div class="black">{wPawn}</div>
+        <div class="white">{wPawn}</div>
+        <div class="black">{wPawn}</div>
+
+        <div class="black">{wRook}</div>
+        <div class="white">{wKnight}</div>
+        <div class="black">{wBishop}</div>
+        <div class="white">{wQueen}</div>
+        <div class="black">{wKing}</div>
+        <div class="white">{wBishop}</div>
+        <div class="black">{wKnight}</div>
+        <div class="white">{wRook}</div>
         </div>
     </div>
   );
